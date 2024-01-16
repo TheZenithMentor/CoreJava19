5ca398f999196a6a205ded78d69746c7c5bc0bcf
GurjarHemendra <connecthemendra29@gmail.com>
2024-01-16 09:17:31 +0530

    OOPS CONCEPT

---
+++ src/main/java/com/corejava/basics/NonStatic/C.java
@@ -0,0 +1,19 @@
+package com.corejava.basics.NonStatic;
+
+public class C {
+    int a;
+
+    {
+        int a=10;
+        System.out.println(this.a);
+    }
+    public static void main(String[] args) {
+        C obj=new C();
+
+        // can i create more than one object of a same class : yes
+        C obj1=new C();
+        System.out.println(obj);
+        System.out.println(obj1);
+
+    }
+}
