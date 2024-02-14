5c5c621cb03c468a75b5edd50c29864589b093ee
GurjarHemendra <connecthemendra29@gmail.com>
2024-02-14 09:04:10 +0530

    avas

--- src/main/java/zenithnxt/programming/basicpattern/Check.java
+++ src/main/java/zenithnxt/programming/basicpattern/Check.java
@@ -4,7 +4,7 @@ public class Check {
     public static void main(String[] args) {
         for (int i=0;i<2;i++)
         {
-            System.out.println("&");
+
             for (int j=0;j<2;j++ )
             {
                 System.out.print("$");
@@ -13,3 +13,42 @@ public class Check {
         }
     }
 }
+/*
+*     for practise
+*  1.     for (int i=0;i<2;i++)
+        {
+
+            for (int j=0;j<2;j++ )
+            {
+                System.out.print("$");
+            }
+            System.out.println("@");
+        }
+*
+*
+* 2.     for (int i=0;i<2;i++)
+        {
+           System.out.print("Q");
+            for (int j=0;j<2;j++ )
+            {
+                System.out.print("$");
+                System.out.println();
+            }
+            System.out.print("@");
+        }
+*
+*
+* 3.     for (int i=0;i<2;i++)
+        {
+          System.out.print("$");
+          System.out.println("$");
+            for (int j=0;j<2;j++ )
+            {
+                System.out.print("$");
+            }
+            System.out.println("@");
+            System.out.println();
+        }
+*
+*
+* */
