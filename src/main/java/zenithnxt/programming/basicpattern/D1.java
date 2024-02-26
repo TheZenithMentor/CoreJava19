557cdeaaea161f9b29b35d3d8aa235e2b768707a
GurjarHemendra <connecthemendra29@gmail.com>
2024-02-26 09:47:13 +0530

    pattern

---
+++ src/main/java/zenithnxt/programming/basicpattern/D1.java
@@ -0,0 +1,14 @@
+package zenithnxt.programming.basicpattern;
+
+public class D1 {
+    public static void main(String[] args) {
+        for (int i=0;i<5;i++)
+        {
+            for(int j=0;j<5;j++)
+            {
+                System.out.print(" ");
+            }
+            for (int k=0;k=j;k)
+        }
+    }
+}
